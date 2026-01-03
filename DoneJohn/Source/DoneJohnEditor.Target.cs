c0dc442e3a9355c4907f2343f40c50ff66845c91
Alex M <153501077+D31TO@users.noreply.github.com>
2026-01-03 20:28:25 +0000

    Updated to UE 5.7.1

--- DoneJohn/Source/DoneJohnEditor.Target.cs
+++ DoneJohn/Source/DoneJohnEditor.Target.cs
@@ -8,7 +8,7 @@ public class DoneJohnEditorTarget : TargetRules
 	public DoneJohnEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("DoneJohn");
 	}
